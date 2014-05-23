871fe7596919aab249de24613c62346576bcf353
crigano <chris.p.rigano@gmail.com>
2014-05-22 21:10:25 -0400

    assignment2 cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix<-function(x = matrix()) is a constructor for an object:
+## e.g, xx<-makeCacheMatrix(x)
+## This Object stores the private member data fields x, and m
+## This Object's get and set method provide access to the data
+## Sets the Data Fields
+## Uses Superassignment <<- operator to maintain the state across function 
+## invocations
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y   
+    m<<-NULL
+  }
+  
+  get<-function() x # get the orignal matrix
+  setmatrix<-function(solve) m<<- solve # inverts the matrix
+  getmatrix<-function() m
+  
+# Group functions into this object to get and set data fields
+  list(set=set, get=get,
+  setmatrix=setmatrix,
+  getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x' 
+## Methods to determine if the data field in m is initialized inmakeCacheMatrix<-function .
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()# returns m from the makeCacheMatrix<-function Object
+  if(!is.null(m)){
+    message("return cached inverse")
+    return(m)
+  }
+  matrix<-x$get() #If not stored in makeCacheMatrix<-function Object, Invert
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m 
 }
+
